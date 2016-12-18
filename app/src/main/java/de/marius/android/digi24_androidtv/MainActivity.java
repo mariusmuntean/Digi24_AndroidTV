22294fc5541e660a42062fcf2a7d1f793cd779cc
Marius <marius.muntean@live.com>
2016-12-18 17:53:52 +0100

    Using OneDrive for stream fallback; Added icon and launcher integration for smartphones

--- app/src/main/java/de/marius/android/digi24_androidtv/MainActivity.java
+++ app/src/main/java/de/marius/android/digi24_androidtv/MainActivity.java
@@ -16,25 +16,68 @@ package de.marius.android.digi24_androidtv;
 
 import android.app.Activity;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Toast;
 import android.widget.VideoView;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.net.URI;
+
 
 /*
  * MainActivity class that loads MainFragment
  */
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
+    private static final String STREAM_URL_FALLBACK = "http://82.76.40.76:80/digi24edge/digi24live/index.m3u8";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        VideoView vv = (VideoView) findViewById(R.id.videoView2);
-        vv.setVideoURI(Uri.parse("http://82.76.40.76:80/digi24edge/digi24live/index.m3u8"));
-        vv.requestFocus();
-        vv.start();
+        new GetLatestStreamUrl().execute();
+    }
+
+
+    private class GetLatestStreamUrl extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // Try to get the current stream URL
+            String streamRepoUrl = getResources().getString(R.string.streamRepositoryUrl);
+
+            String currentStreamUrl = null;
+            try {
+                currentStreamUrl = IOUtils.toString(URI.create(streamRepoUrl));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return currentStreamUrl;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            boolean shouldUseFallback = result == null || result.length() == 0;
+
+            if (shouldUseFallback) {
+                Toast.makeText(MainActivity.this, "Trying fallback", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Using: " + result, Toast.LENGTH_LONG).show();
+            }
+
+            // Fallback if none found
+            String currentStreamUrl = shouldUseFallback ? STREAM_URL_FALLBACK : result;
+
+            // Set the videoview source and start
+            VideoView vv = (VideoView) findViewById(R.id.videoView2);
+            vv.setVideoURI(Uri.parse(currentStreamUrl));
+            vv.requestFocus();
+            vv.start();
+        }
     }
 }
